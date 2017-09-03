0cf614f1f447e6335d9a69074ad550eca5c64bbe
David Skaggs <davidtskaggs@gmail.com>
2017-09-03 12:32:45 -0400

    Further refactor sorting method

--- sort.rb
+++ sort.rb
@@ -1,8 +1,5 @@
 # Rewrite this method with your own sorting algorithm
 # Make your algorithm pass each of the tests in spec/sort_spec.rb
 def sort(objects)
-  if objects.class != Array
-    objects.split(" ")
-  end
-    objects.sort_by(&:to_s)
+  objects.sort_by(&:to_s)
 end
