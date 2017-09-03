f7888fb7115cc15181f8f2996c986782dad405c5
Andres Arias <email": null>
2017-09-03 16:31:55 -0400

    Bug Fix for mean.rb and sort.rb

--- sort.rb
+++ sort.rb
@@ -1,5 +1,10 @@
 # Rewrite this method with your own sorting algorithm
 # Make your algorithm pass each of the tests in spec/sort_spec.rb
+
+
 def sort(objects)
-  objects.sort
+  objects.sort_by{|x| x.to_s}
 end
+
+
+
