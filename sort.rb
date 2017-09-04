8e35b2bc5f126ef1c8c89d072acf1c78c09dd2d9
klorealf <klorealf@gmail.com>
2017-09-04 19:59:16 -0400

    add comparison operator to sort method

--- sort.rb
+++ sort.rb
@@ -1,5 +1,8 @@
 # Rewrite this method with your own sorting algorithm
 # Make your algorithm pass each of the tests in spec/sort_spec.rb
 def sort(objects)
-  objects.sort
+	objects.sort do |a,b|
+		a.to_s <=> b.to_s
+	end
 end
+
