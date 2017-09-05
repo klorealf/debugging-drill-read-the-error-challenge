be82b514eb04f07121ae1b4db4c7771afaf76c4a
Catherine Rice <clr2107@gmail.com>
2017-09-04 23:14:33 -0400

    Debug mean and sort methods

--- sort.rb
+++ sort.rb
@@ -1,5 +1,5 @@
 # Rewrite this method with your own sorting algorithm
 # Make your algorithm pass each of the tests in spec/sort_spec.rb
 def sort(objects)
-  objects.sort
+ objects.sort_by{|obj| obj.to_s}
 end
